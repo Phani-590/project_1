8bbe46f6a25fddc59cff42127f578ec8a1d1b612
Phani-590 <crazyfani590@gmail.com>
2025-02-26 22:50:59 +0530

    Add files via upload

---
+++ Controllers/consumerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Farmer_To_Customer.Controllers
+{
+    public class consumerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Farmer_To_Customer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
